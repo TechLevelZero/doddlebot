2cf933a3c2e34379b1424f19ad17356f436c6c94
TechLevelZero <techlevelzero@Bens-MacBook-Pro.local>
2021-05-07 21:59:52 +0100

    fix

--- modules/tools/member_init.ts
+++ modules/tools/member_init.ts
@@ -3,13 +3,13 @@ const bot = require('../../json_files/data.json')
 
 import channel from '../global/channel'
 
-export default function message_init(message) {
+export default function message_init(member) {
 
     const embed = new MessageEmbed()
         .setColor(0xFEF65B)
         .setTitle('**Welcome to doddlecord!** ' + member.user.username)
         .setDescription(bot.text.welcomemsg);
-    channel(message, 'introduceyourself').send({embed}).then(msg => {
+    channel(member, 'introduceyourself').send({embed}).then(msg => {
         setTimeout(() => {
             msg.delete({ timeout: 1, reason: 'bot removed'})
         }, 300000);
