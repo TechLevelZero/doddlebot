2ee660a3ef708fd950860c5feee89a6d1a61bf49
TechLevelZero <techlevelzero@Bens-MacBook-Pro.local>
2021-05-07 22:09:50 +0100

    added time out

--- modules/tools/member_init.ts
+++ modules/tools/member_init.ts
@@ -4,15 +4,16 @@ const bot = require('../../json_files/data.json')
 import channel from '../global/channel'
 
 export default function message_init(member) {
-
-    const embed = new MessageEmbed()
-        .setColor(0xFEF65B)
-        .setTitle('**Welcome to doddlecord!** ' + member.user.username)
-        .setDescription(bot.text.welcomemsg);
-    channel(member, 'introduceyourself').send({embed}).then(msg => {
-        setTimeout(() => {
-            msg.delete({ timeout: 1, reason: 'bot removed'})
-        }, 300000);
-    })
+    setTimeout(() => { // time out is need or new members can not see the message 
+        const embed = new MessageEmbed()
+            .setColor(0xFEF65B)
+            .setTitle('**Welcome to doddlecord!** ' + member.user.username)
+            .setDescription(bot.text.welcomemsg);
+        channel(member, 'introduceyourself').send({embed}).then(msg => {
+            setTimeout(() => {
+                msg.delete({ timeout: 1, reason: 'bot removed'})
+            }, 300000);
+        })  
+    }, 2500);
 }
 
